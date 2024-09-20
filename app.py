b1719f60163facb968cc46719f5e2f4f32a26472
Jesly Joji <133549580+Jesly-Joji@users.noreply.github.com>
2024-09-20 14:14:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import torch
+import requests
+
 
 st.title("Daisy/ Dandellion Flower Classifcation")
 
@@ -6,3 +9,12 @@ uploaded_image = st.file_uploader("Upload the Image", type=["png","jpeg","jpg"])
 
 if uploaded_image:
    st.image(uploaded_image)
+
+
+model_url="https://raw.githubusercontent.com/Jesly-Joji/Flower-Image-Classification/main/cnn_image_classification_model.pt"
+
+response = requests.get(model_url)
+with open("model.pt", "wb") as f:
+    f.write(response.content)
+
+torch.load("model.pt")
