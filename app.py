8007c7dab4846789875523a5c50404abaedcd974
Jesly Joji <133549580+Jesly-Joji@users.noreply.github.com>
2024-09-20 11:13:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.title("Daisy/ Dandellion Flower Classifcation")
 
 uploaded_image = st.file_uploader("Upload the Image", type=["png","jpeg","jpg"])
 
-st.image(uploaded_img)
+st.image(uploaded_image)
